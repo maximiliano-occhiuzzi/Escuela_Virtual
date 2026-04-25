6d8b5663740a862f1e9c46512050eacdb4be9bf5
Sebastiianpz <sebastiandavalos09@gmail.com>
2026-04-25 19:07:15 -0300

    Pagina Login, App queda como rutador, carpeta node incluida 25/4

---
+++ src/pages/Login.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import "./Login.css";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+  e.preventDefault();
+
+  if (!email.endsWith("@fatimarem.edu.ar")) {
+    alert("Correo inválido");
+    return;
+  }
+
+  const usuario = email.split("@")[0];
+
+  // 🔥 Detectar si es número (alumno)
+  if (!isNaN(usuario)) {
+    alert("Entraste como ALUMNO");
+  } else {
+    alert("Entraste como DOCENTE");
+  }
+};
+
+  return (
+    <div className="login-bg">
+      <div className="login-card shadow">
+        <h2 className="text-center mb-4">Sistema Escolar</h2>
+
+        <form onSubmit={handleLogin}>
+          <input
+            type="email"
+            className="form-control mb-3"
+            placeholder="Correo electrónico"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <input
+            type="password"
+            className="form-control mb-3"
+            placeholder="Contraseña"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button className="btn btn-primary w-100">
+            Ingresar
+          </button>
+        </form>
+
+        <p className="text-center mt-3 small text-muted">
+          Docente: sebastiandavalos@fatimarem.edu.ar <br />
+          Alumno: 48318493@fatimarem.edu.ar
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
